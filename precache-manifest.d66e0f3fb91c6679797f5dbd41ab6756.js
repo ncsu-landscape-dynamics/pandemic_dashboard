80752efaa237891824914005a426ffebad0be495
gcmillar <gcmillar@ncsu.edu>
2020-06-29 23:17:14 -0400

    Updates

---
+++ precache-manifest.d66e0f3fb91c6679797f5dbd41ab6756.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d34978a1d11c494cc5d5b5d387e0cc5",
+    "url": "/pandemic_dashboard/index.html"
+  },
+  {
+    "revision": "d68e87efc5781b950826",
+    "url": "/pandemic_dashboard/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "9d23d0437c799647d923",
+    "url": "/pandemic_dashboard/static/js/2.54eab1ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pandemic_dashboard/static/js/2.54eab1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68e87efc5781b950826",
+    "url": "/pandemic_dashboard/static/js/main.500b65a6.chunk.js"
+  },
+  {
+    "revision": "c1dc54c43f91ef373ea3",
+    "url": "/pandemic_dashboard/static/js/runtime-main.ed90702f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pandemic_dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
