7bc0b43986be5d1855ee827849e2b7c120d86b06
gcmillar <gcmillar@ncsu.edu>
2020-07-01 17:36:50 -0400

    small display changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pandemic_dashboard/precache-manifest.4ced5e1bcc3d112f8da570b2bba582d1.js"
+  "/pandemic_dashboard/precache-manifest.17a30de8036f93ad9a5649f6c3c0f7a0.js"
 );
 
 self.addEventListener('message', (event) => {
