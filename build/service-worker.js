9011ee22203a47733173dba547de1a57f4347cb9
gcmillar <gcmillar@ncsu.edu>
2020-07-22 17:05:25 -0400

    major UI changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pandemic_dashboard/precache-manifest.1e460b967ecbea1a2ab6c31422ebad5d.js"
+  "/pandemic_dashboard/precache-manifest.de66ade8edc17c865b7f4f2130630926.js"
 );
 
 self.addEventListener('message', (event) => {
