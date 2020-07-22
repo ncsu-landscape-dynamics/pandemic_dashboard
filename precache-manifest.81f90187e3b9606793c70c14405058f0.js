8156d4ce384f3e4daecf2654db9d9bf631ef272c
gcmillar <gcmillar@ncsu.edu>
2020-07-22 17:20:39 -0400

    Updates

---
+++ precache-manifest.81f90187e3b9606793c70c14405058f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7d852ae4de503b3e2f69e200cc8368",
+    "url": "/pandemic_dashboard/index.html"
+  },
+  {
+    "revision": "9aa48a597d1bfb44e724",
+    "url": "/pandemic_dashboard/static/css/2.d46cd15e.chunk.css"
+  },
+  {
+    "revision": "73f067e8450b1bd47c3f",
+    "url": "/pandemic_dashboard/static/css/main.d0461a72.chunk.css"
+  },
+  {
+    "revision": "9aa48a597d1bfb44e724",
+    "url": "/pandemic_dashboard/static/js/2.b7ef56f9.chunk.js"
+  },
+  {
+    "revision": "b88bca802d90b4a3030e9a05cb0ffaf7",
+    "url": "/pandemic_dashboard/static/js/2.b7ef56f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f067e8450b1bd47c3f",
+    "url": "/pandemic_dashboard/static/js/main.b6eefe75.chunk.js"
+  },
+  {
+    "revision": "c1dc54c43f91ef373ea3",
+    "url": "/pandemic_dashboard/static/js/runtime-main.ed90702f.js"
+  },
+  {
+    "revision": "1094cb6db0d2f1d9ced2802dd6a11c3d",
+    "url": "/pandemic_dashboard/static/media/logo.1094cb6d.svg"
+  }
+]);
