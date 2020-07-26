4839b53862d26761e89edf3ca5f1922848bd4e65
gcmillar <gcmillar@ncsu.edu>
2020-07-26 17:45:20 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pandemic_dashboard/precache-manifest.9f6b32407410d3a0c48f7b58df7eaea4.js"
+  "/pandemic_dashboard/precache-manifest.ad361ef0d52009f0d013f5d498f38b7e.js"
 );
 
 self.addEventListener('message', (event) => {
